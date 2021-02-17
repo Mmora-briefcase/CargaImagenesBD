1c7d46599ec4fd867f14fa3640902ff2dc45cea0
Marcelo <mmorap89@gmail.com>
2021-02-17 09:52:23 -0600

    Se crea Listado y Vista previa

--- app/app/Controllers/PersonasController.cs
+++ app/app/Controllers/PersonasController.cs
@@ -20,5 +20,33 @@ namespace app.Controllers
 
             return View(listado);
         }
+
+        public ActionResult Vermas(int id)
+        {
+            var model = new Persona();
+            var db = new cargaarchivostestEntities();
+
+            var opersonadb = db.Persona.Find(id);
+
+            model.Id = opersonadb.Id;
+            model.Nombre = opersonadb.Nombre;
+            model.Historia = opersonadb.Historia;
+            
+
+
+            return View(model);
+        }
+
+        public ActionResult Nuevo( )
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Nuevo(Persona model)
+        {
+
+            return View();
+        }
     }
 }
