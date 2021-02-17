4b7d3da7258d7ac6bd73901f674236e985d4618d
Marcelo <mmorap89@gmail.com>
2021-02-17 08:55:39 -0600

    Crear un Listado en el controler , recorrerlo en la vista dentro de un Jumbotron y no una Tabla

---
+++ app/app/Controllers/PersonasController.cs
@@ -0,0 +1,24 @@
+﻿using app.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace app.Controllers
+{
+    public class PersonasController : Controller
+    {
+        // GET: Personas
+        public ActionResult Index()
+        {
+
+            var db = new cargaarchivostestEntities();
+
+            var listado = from x in db.Persona
+                          select x;
+
+            return View(listado);
+        }
+    }
+}
